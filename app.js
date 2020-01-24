427524f7e180559a64d5bff06e22e245fdc66267
Caleb Duke <dukecj18@gmail.com>
2020-01-24 14:03:23 -0600

    dom

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     button.appendChild(btnText);
     document.body.appendChild(button);
-    
+
     let containerDiv = document.createElement('div');
-    containerDiv.classList.add('container');
+    containerDiv.className = 'container';
     containerDiv.style.border = "thick solid #0000FF";
     document.body.appendChild(containerDiv);
-    
-    
+
+    let counter = 0
+
     button.addEventListener('click', function () {
+        console.log(counter);
         let box = document.createElement('div');
-        box.classList.add('square');
+        box.className = 'square';
+        box.id = counter;
+        let idDisplay = document.createElement('span');
+        idDisplay.className = 'id-display';
+        idDisplay.innerText = counter;
         containerDiv.appendChild(box);
+        counter++;
+
+        box.appendChild(idDisplay);
     });
 });
